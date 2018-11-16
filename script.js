7bc26eaaef195ce6669e43b7167c17d33e457d9b
novaQuach <nova.quach@gmail.com>
2018-11-15 22:53:27 -0500

    most picture array updated and linked

--- script.js
+++ script.js
@@ -1,78 +1,154 @@
 const data = {
     "circular": {
         "solids": {
-            "regular":[],
-            "oversized-polygon":[],
+            "regular":
+                [
+                    'assets/circular-solid-regular-images/circular-solid-regular-image-1.jpg',
+                    'assets/circular-solid-regular-images/circular-solid-regular-image-2.jpg',
+                    'assets/circular-solid-regular-images/circular-solid-regular-image-3.jpg',
+                    'assets/circular-solid-regular-images/circular-solid-regular-image-4.jpg'
+                ],
+
+            "oversized":
+                [
+                    'assets/circular-solid-oversized-images/circular-solid-oversized-image-1.jpg',
+                    'assets/circular-solid-oversized-images/circular-solid-oversized-image-2.jpg',
+                    'assets/circular-solid-oversized-images/circular-solid-oversized-image-3.jpg',
+                    'assets/circular-solid-oversized-images/circular-solid-oversized-image-4.jpg'
+                ]
         },
         "metals": {
-            "regular":[],
-            "oversized-polygon":[],
+            "regular":
+                [
+                    'assets/circular-metal-regular-images/circular-metal-regular-image-1.jpg',
+                    'assets/circular-metal-regular-images/circular-metal-regular-image-2.jpg',
+                    'assets/circular-metal-regular-images/circular-metal-regular-image-3.jpg',
+                    'assets/circular-metal-regular-images/circular-metal-regular-image-4.jpg',
+                    'assets/circular-metal-regular-images/circular-metal-regular-image-5.jpg'
+                ],
+
+            "oversized":
+                [
+                    'assets/circular-metal-oversized-images/circular-metal-oversized-image-1.jpg',
+                    'assets/circular-metal-oversized-images/circular-metal-oversized-image-2.jpg',
+                    'assets/circular-metal-oversized-images/circular-metal-oversized-image-3.jpg',
+                    'assets/circular-metal-oversized-images/circular-metal-oversized-image-4.jpg'
+                ],
         },
 
         "patterns": {
-            "regular": [],
-            "oversized-polygon": [],
+            "regular":
+                [
+                    'assets/circular-pattern-regular-images/circular-pattern-regular-image-1.jpg',
+                    'assets/circular-pattern-regular-images/circular-pattern-regular-image-2.jpg',
+                    'assets/circular-pattern-regular-images/circular-pattern-regular-image-3.jpg'
+                ],
+
+            "oversized":
+                [
+                    'assets/circular-pattern-oversized-images/circular-pattern-oversized-image-1.jpg',
+                    'assets/circular-pattern-oversized-images/circular-pattern-oversized-image-2.jpg',
+                    'assets/circular-pattern-oversized-images/circular-pattern-oversized-image-3.jpg',
+                    'assets/circular-pattern-oversized-images/circular-pattern-oversized-image-4.jpg'
+                ],
         },
 
         "transparent": {
             "regular": [],
-            "oversized-polygon": [],    
+            "oversized":
+                [
+                    'assets/circular-transparent-oversized-images/circular-transparent-oversized-image-1.jpg',
+                    'assets/circular-transparent-oversized-images/circular-transparent-oversized-image-2.jpg',
+                    'assets/circular-transparent-oversized-images/circular-transparent-oversized-image-3.jpg',
+                    'assets/circular-transparent-oversized-images/circular-transparent-oversized-image-4.jpg'
+                ],
         }
     },
     "rectangular": {
         "solids": {
-            "regular": [],
-            "oversized-polygon": [],
+            "regular": 
+                [
+                    'assets/rectangular-solid-regular-images/rectangular-solid-regular-image-1.jpg',
+                    'assets/rectangular-solid-regular-images/rectangular-solid-regular-image-2.jpg',
+                    'assets/rectangular-solid-regular-images/rectangular-solid-regular-image-3.jpg'
+                ],
+
+            "oversized":
+                [
+                    'assets/rectangular-solid-oversized-images/rectangular-solid-oversized-image-1.jpg',
+                    'assets/rectangular-solid-oversized-images/rectangular-solid-oversized-image-2.jpg', 'assets/rectangular-solid-oversized-images/rectangular-solid-oversized-image-3.jpg',
+                    'assets/rectangular-solid-oversized-images/rectangular-solid-oversized-image-4.jpg'
+                ],
         },
         "metals": {
-            "regular": [],
-            "oversized-polygon": [],
+            "regular":
+                [
+                'assets/rectangular/metal-regular-images/rectangular-metal-regular-image-1.jpg'
+                ],
+            "oversized": [],
         },
 
         "patterns": {
             "regular": [],
-            "oversized-polygon": [],
+            "oversized":
+                ['assets/rectangular-pattern-oversized-images/rectangular-pattern-oversized-image-1.jpg',
+                    'assets/rectangular-pattern-oversized-images/rectangular-pattern-oversized-image-2.jpg'],
         },
 
         "transparent": {
-            "regular": [],
-            "oversized-polygon": [],   
-        } 
+            "regular":
+                [
+                    'assets/rectangular-transparent-regular-images/rectangular-transparent-regular-image-1.jpg',
+                    'assets/rectangular-transparent-regular-images/rectangular-transparent-regular-image-2.jpg',
+                    'assets/rectangular-transparent-regular-images/rectangular-transparent-regular-image-3.jpg',
+                    'assets/rectangular-transparent-regular-images/rectangular-transparent-regular-image-4.jpg'
+                ],
+            "oversized": 
+                [
+                    'assets/rectangular-transparent-oversized-images/rectangular-transparent-oversized-image-1.jpg',
+                    'assets/rectangular-transparent-oversized-images/rectangular-transparent-oversized-image-2.jpg',
+                    'assets/rectangular-transparent-oversized-images/rectangular-transparent-oversized-image-3.jpg',
+                    'assets/rectangular-transparent-oversized-images/rectangular-transparent-oversized-image-4.jpg'
+
+                ],
+        }
     },
-    "whatever": 
-        ['assets/whatever-images/whatever-image-1.jpg', 'assets/whatever-images/whatever-image-2.jpg', 'assets/whatever-images/whatever-image-3.jpg', 'assets/whatever-images/whatever-image-4.jpg', 'assets/whatever-images/whatever-image-5.jpg','assets/whatever-images/whatever-image-6.jpg']
-    
-        
+    "whatever":
+        [
+            'assets/whatever-images/whatever-image-1.jpg', 'assets/whatever-images/whatever-image-2.jpg', 'assets/whatever-images/whatever-image-3.jpg', 'assets/whatever-images/whatever-image-4.jpg', 'assets/whatever-images/whatever-image-5.jpg', 'assets/whatever-images/whatever-image-6.jpg'
+    ]
+
+
 
 };
 
 
-$(function(){
+$(function () {
 
-// figure out submit button on click 
-// save responses for each question 
-// write function to push all user responses to an array save into userPreferences 
-// dynamic ? filter function to get the results we want before concating the desired arrays 
-// take their repsective arrays and concatenate into their options array 
-// userGlasses function will take a tailored array ( array that combines all the glasses option that'll potentailly fit the user) it will return a random pair of glasses 
-//go back to submit button event --> link the url of the selected option to the displayed img to show user their glasses suggestion 
-
-
-    const userPreferences=[]
+    // figure out submit button on click 
+    // save responses for each question 
+    // write function to push all user responses to an array save into userPreferences 
+    // dynamic ? filter function to get the results we want before concating the desired arrays 
+    // take their repsective arrays and concatenate into their options array 
+    // userGlasses function will take a tailored array ( array that combines all the glasses option that'll potentailly fit the user) it will return a random pair of glasses 
+    //go back to submit button event --> link the url of the selected option to the displayed img to show user their glasses suggestion 
 
 
-    $('form').on('submit',function(event){
+    const userPreferences = []
+
+
+    $('form').on('submit', function (event) {
         const userShape = $("input[name='shape']:checked").val();
         const userVibe = $("input[name='vibe']:checked").val();
         const userPersonality = $("input[name='personality']:checked").val();
         const userCare = $("input[name='care']:checked").val();
         event.preventDefault();
-        userPreferences.push(userShape, userVibe, userPersonality,userCare);
+        userPreferences.push(userShape, userVibe, userPersonality, userCare);
         console.log(userPreferences);
-        let glassesChoices = getGlassesChoices(userShape,userVibe,userPersonality); //getGlasses returns an array
+        let glassesChoices = getGlassesChoices(userShape, userVibe, userPersonality); //getGlasses returns an array
 
         let glasses = getGlasses(glassesChoices);
-        showResults(userCare,glasses);
+        showResults(userCare, glasses);
 
     }); //end of onSubmit function 
 
@@ -84,7 +160,7 @@ $(function(){
         let glassesShape, glassesSize, glassesFinish = userVibe;
 
         if (userPersonality === "adventurous") {
-            glassesSize = 'oversized-polygon';
+            glassesSize = 'oversized';
         } else {
             glassesSize = 'regular';
         };
@@ -101,12 +177,12 @@ $(function(){
     };
 
 
-    const getGlasses = function (glassesChoices){
-        return glassesChoices[Math.floor(Math.random()*glassesChoices.length)]
+    const getGlasses = function (glassesChoices) {
+        return glassesChoices[Math.floor(Math.random() * glassesChoices.length)]
     };
 
-    
-    const showResults = function (userCare,glasses){
+
+    const showResults = function (userCare, glasses) {
         if (userCare === 'yes-care') {
             $('.yourGlasses').html(`<img src="${glasses}" alt="picture of glasses"><img>`);
 
